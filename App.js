3467253b46b7b47dc42bc0c61f35c0b0896b18bf
Jed <jedreya@bu.edu>
2021-09-15 03:47:54 +0900

    Hello World

--- App.js
+++ App.js
@@ -2,13 +2,27 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'World!',
+    }
+  }
+
+  onClick = () => {
+    this.setState({
+      name: 'Naoto!',
+    })
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.nameText}>Hello {this.state.name}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
