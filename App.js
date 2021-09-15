43c4c2821141bd3aaa81c2e23258083e65026ade
Jed <jedreya@bu.edu>
2021-09-15 11:25:04 +0900

    Created app, added header, added buttons and styling

--- App.js
+++ App.js
@@ -1,26 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, SafeAreaView, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'World!',
-    }
-  }
-
-  onClick = () => {
-    this.setState({
-      name: 'Naoto!',
-    })
-  };
-
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.nameText}>Hello {this.state.name}</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <SafeAreaView style={styles.titlecenter}>
+          <Text style={styles.title}>EC463 SW MiniProject</Text>
+        </SafeAreaView>
+        <SafeAreaView style={styles.buttoncontainer}>
+          <SafeAreaView>
+            <TouchableOpacity style={styles.buttons} onPress={() => console.log("button1 tapped")}><Text> Log Out </Text></TouchableOpacity>
+          </SafeAreaView>
+          <SafeAreaView>
+            <TouchableOpacity style={styles.buttons} onPress={() => console.log("button2 tapped")}><Text> Scan Item </Text></TouchableOpacity>
+          </SafeAreaView>
+          <SafeAreaView>
+            <TouchableOpacity style={styles.buttons} onPress={() => console.log("button3 tapped")}><Text> View Profile </Text></TouchableOpacity>
+          </SafeAreaView>
+        </SafeAreaView>
+
+
+      </SafeAreaView >
     );
   }
 }
@@ -29,7 +31,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  title: {
+    marginTop: 100,
+    fontSize: 25,
+  },
+  titlecenter: {
+    alignItems: 'center'
+  },
+  buttoncontainer: {
+    flex: 1,
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+  buttons: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10
+  }
 });
